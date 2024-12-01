36122518ff6383205eca967c414c4af241623840
quadeer2003 <quadeer2003@gmail.com>
2024-12-01 19:17:17 +0530

    add speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { ThemeProvider } from "@/components/providers/theme";
 
@@ -24,6 +25,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <SpeedInsights />
+
           </ThemeProvider>
       </body>
     </html>
