b82336b0903c4b6355e984d0a8867fabda6b2910
Mohammad Abdul Quadeer <143788753+quadeer2003@users.noreply.github.com>
2025-03-22 18:18:31 +0530

    RESET

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatar.vercel.sh',
-        pathname: '/**',
-      },
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+  /* config options here */
 };
 
 export default nextConfig;
